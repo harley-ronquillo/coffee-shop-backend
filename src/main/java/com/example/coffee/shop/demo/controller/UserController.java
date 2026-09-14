a93af83c76d6ccfacc77c832c8af34cc413a3162
harley-ronquillo <harleyronquillo@gmail.com>
2026-09-14 10:56:03 +0800

    implemented response entity as return type in controller

--- src/main/java/com/example/coffee/shop/demo/controller/UserController.java
+++ src/main/java/com/example/coffee/shop/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.coffee.shop.demo.controller;
 import com.example.coffee.shop.demo.model.dto.UserRequest;
 import com.example.coffee.shop.demo.model.dto.UserResponse;
 import com.example.coffee.shop.demo.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,34 +12,39 @@ import java.util.List;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
-    private UserService service;
+    private final UserService service;
 
     public UserController(UserService service){
         this.service = service;
     }
 
     @PostMapping
-    public UserResponse createUser(@RequestBody UserRequest request){
-        return service.createUser(request);
+    public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request){
+        return ResponseEntity.ok()
+                .body(service.createUser(request));
     }
 
     @PutMapping("/$id")
-    public UserResponse updateUser(@RequestParam Long id, @RequestBody UserRequest request){
-        return service.updateUser(id, request);
+    public ResponseEntity<UserResponse> updateUser(@RequestParam Long id, @RequestBody UserRequest request){
+        return  ResponseEntity.ok()
+                .body(service.updateUser(id, request));
     }
 
     @GetMapping
-    public List<UserResponse> getAllUsers(){
-        return service.findAllUser();
+    public ResponseEntity<List<UserResponse>> getAllUsers(){
+        return ResponseEntity.ok()
+                .body(service.findAllUser());
     }
 
     @GetMapping("/$id")
-    public UserResponse getUserById(@RequestParam Long id){
-        return service.findUserById(id);
+    public ResponseEntity<UserResponse> getUserById(@RequestParam Long id){
+        return ResponseEntity.ok()
+                .body(service.findUserById(id));
     }
 
     @DeleteMapping
-    public UserResponse deleteUser(@RequestParam Long id){
-        return service.deleteUser(id);
+    public ResponseEntity<UserResponse> deleteUser(@RequestParam Long id){
+        return ResponseEntity.ok()
+                .body(service.deleteUser(id));
     }
 }
